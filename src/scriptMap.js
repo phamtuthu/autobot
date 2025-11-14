5be606552aba68264acb52dbcf45c64b28fd768c
phamtuthu <thupt@ghn.vn>
2025-11-14 11:47:02 +0700

    Create scriptMap.js

---
+++ src/scriptMap.js
@@ -0,0 +1,5 @@
+export const SCRIPT_MAP = {
+  "import_lead": "https://script.google.com/macros/s/XXXX/exec?run=importLead",
+  "import_cost": "https://script.google.com/macros/s/YYYY/exec?run=syncCost",
+  "import_aff":  "https://script.google.com/macros/s/ZZZZ/exec?run=importAff"
+};
