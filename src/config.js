de0b73f3c5892e26f5f0cb29560d12cfcfb592f2
phamtuthu <thupt@ghn.vn>
2025-11-14 11:46:16 +0700

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const BOT_TOKEN = process.env.TELEGRAM_TOKEN;
+export const TG_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+
+export const GAS_SECRET = process.env.GAS_SECRET || "ghn_secret_key";
